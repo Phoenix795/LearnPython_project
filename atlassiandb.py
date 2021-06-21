97389a467381cb2df3e5b1e5802e618c617edf63
Phoenix795 <45521180+Phoenix795@users.noreply.github.com>
2021-06-21 23:28:59 +0600

    Atlassain DB model v1 (#2)
    * Atlassain DB model v1
    
    * Fixes after review on 06/10/2021
    
    * adding relationship-fields
    
    * some fixes
    
    * adding resolved_date field
    
    * fixies after review on 06/20/2021
    
    * fixies after review on 06/21/2021

---
+++ atlassiandb.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm.session import Session
+import settings
+
+engine = create_engine(settings.DBMS_LINK)
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property
---
+++ dbmodels.py
@@ -0,0 +1,124 @@
+from sqlalchemy import Column, Integer, String, Date, VARCHAR, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.expression import false
+from atlassiandb import Base, engine
+
+
+class Type(Base):
+    __tablename__ = "type"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(255),unique=True)
+
+    def __repr__(self) -> str:
+        return f"Type name {self.id}, {self.name}"
+
+
+class Status(Base):
+    __tablename__ = "status"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(255),unique=True)
+
+    def __repr__(self) -> str:
+        return f"Status name {self.id}, {self.name}"
+
+
+class Priority(Base):
+    __tablename__ = "priority"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(255),unique=True)
+
+    def __repr__(self) -> str:
+        return f"Priority name {self.id}, {self.name}"
+
+
+class Resolution(Base):
+    __tablename__ = "resolution"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(255),unique=True)
+
+    def __repr__(self) -> str:
+        return f"Resolution name {self.id}, {self.name}"
+
+
+class LinkType(Base):
+    __tablename__ = "link_type"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(255),unique=True)
+
+    def __repr__(self) -> str:
+        return f"Link type name {self.id}, {self.name}"
+
+
+class Issue(Base):
+    __tablename__ = "issue"
+
+    id = Column(Integer, primary_key=True)
+    key = Column(VARCHAR(255),unique=True, nullable=False)
+    summary = Column(String(255), nullable=False)
+
+    type_id = Column(Integer, ForeignKey('type.id'), nullable=False)
+    type = relationship('Type', backref='issues')
+    
+    status_id = Column(Integer, ForeignKey('status.id'), nullable=False)
+    status = relationship('Status', backref='issues')
+    
+    priority_id = Column(Integer, ForeignKey('priority.id'))
+    priority = relationship('Priority', backref='issues')
+
+    resolution_id = Column(Integer, ForeignKey('resolution.id'))
+    resolution = relationship('Resolution', backref='issues')
+
+    description = Column(String(10000))
+    votes_quantity = Column(Integer,default=0)
+    watchers_quantity = Column(Integer,default=0)
+    fixed_version = Column(String(255))
+    affected_version = Column(String(255))
+    created_date = Column(Date, nullable=False)
+    updated_date = Column(Date)
+    resolved_date = Column(Date)
+
+    def __repr__(self) -> str:
+        return f"Issue Key {self.id}, {self.key}"
+
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    id = Column(Integer, primary_key=True)
+    
+    issue_id = Column(Integer, ForeignKey('issue.id'), nullable=False, index=True)
+    issue = relationship('Issue', backref='comments', lazy=True)
+    
+    content = Column(String(500))
+    date_created = Column(Date, nullable=False)
+    creator = Column(String(255))
+
+    def __repr__(self) -> str:
+        return f"Comment id {self.id}"
+
+
+class IssueLink(Base):
+    __tablename__ = "issue_link"
+
+    id = Column(Integer, primary_key=True)
+
+    child_issue_id = Column(Integer, ForeignKey('issue.id'), nullable=False, index=True)
+    child_issue = relationship('Issue', backref='child_issue_links', lazy=True)
+
+    parent_issue_id = Column(Integer, ForeignKey('issue.id'), nullable=False, index=True)
+    parent_issue = relationship('Issue', backref='parent_issue_links', lazy=True)
+
+    type_id = Column(Integer, ForeignKey('link_type.id'), nullable=False)
+    type = relationship('LinkType', backref='issue_links')
+
+    def __repr__(self) -> str: 
+        return f"Issue_link id {self.id}" 
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
