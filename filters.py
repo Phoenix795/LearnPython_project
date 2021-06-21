03762642a74f78044597f186ff444556fa9a2957
Alexey Lonshakov <lonshakovaleksei@gmail.com>
2021-06-21 18:10:16 +0600

    fixies after review on 06/20/2021

--- filters.py
+++ filters.py
@@ -1,23 +1,24 @@
-B_CLSD_LOW_NEC_EFV = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Low and component is not EMPTY and fixVersion is EMPTY'
-B_CLSD_LOW_NEC_NEFV = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Low and component is not EMPTY and fixVersion is not EMPTY'
-B_CLSD_LOW_EC = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Low and component is EMPTY'
-B_CLSD_MED_EC = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Medium AND component is EMPTY'
-B_CLSD_MED_NEC = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Medium AND component is not EMPTY'
-B_CLSD_HIGH = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority in (Highest, High)'
-B_NT = 'project = JRASERVER AND issuetype = Bug AND status = "Needs Triage"'
-B_IN_OTHER_S = 'project = JRASERVER AND issuetype = Bug AND status in ("In Progress", "In Review", "Gathering Impact", "Long Term Backlog", "Short Term Backlog", "Waiting for Release")'
-S_CLSD_EL_NEC = 'project = JRASERVER AND issuetype = Suggestion AND status = Closed AND labels is EMPTY AND component is not EMPTY'
-S_CLSD_EL_EC = 'project = JRASERVER AND issuetype = Suggestion AND status = Closed AND labels is EMPTY AND component is EMPTY'
-S_CLSD_NEL_EC = 'project = JRASERVER AND issuetype = Suggestion AND status = Closed AND labels is not EMPTY AND component is EMPTY'
-S_CLSD_NEL_NEL = 'project = JRASERVER AND issuetype = Suggestion AND status = Closed AND labels is not EMPTY AND component is not EMPTY'
-S_GI_EL = 'project = JRASERVER AND issuetype = Suggestion AND status = "Gathering Interest" and labels is EMPTY'
-S_GI_NEL = 'project = JRASERVER AND issuetype = Suggestion AND status = "Gathering Interest" and labels is not EMPTY'
-S_IN_OTHER_S = 'project = JRASERVER AND issuetype = Suggestion AND status in ("In Progress", Reviewing, "Under Consideration", "Future Consideration", "Not Being Considered", "Waiting for Release")'
-ALL_VULNARAB ='project = JRASERVER AND issuetype = "Public Security Vulnerability"'
+CLOSED_LOW_BUGS_WITH_COMPONENTS = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Low and component is not EMPTY and fixVersion is EMPTY'
+CLOSED_LOW_BUGS_WITH_COMPONENTS_AND_FIXES = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Low and component is not EMPTY and fixVersion is not EMPTY'
+CLOSED_LOW_BUGS = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Low and component is EMPTY'
+CLOSED_MEDIUM_BUGS = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Medium AND component is EMPTY'
+CLOSED_MEDIUM_BUGS_WITH_COMPONENTS = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority = Medium AND component is not EMPTY'
+CLOSED_HIGH_BUGS = 'project = JRASERVER AND issuetype = Bug AND status = Closed AND priority in (Highest, High)'
+NEEDS_TRIAGE_BUGS = 'project = JRASERVER AND issuetype = Bug AND status = "Needs Triage"'
+OTHERS_BUGS = 'project = JRASERVER AND issuetype = Bug AND status in ("In Progress", "In Review", "Gathering Impact", "Long Term Backlog", "Short Term Backlog", "Waiting for Release")'
+CLOSED_SUGGESTIONS_WITH_COMPONENTS = 'project = JRASERVER AND issuetype = Suggestion AND status = Closed AND labels is EMPTY AND component is not EMPTY'
+CLOSED_SUGGESTIONS = 'project = JRASERVER AND issuetype = Suggestion AND status = Closed AND labels is EMPTY AND component is EMPTY'
+CLOSED_SUGGESTIONS_WITH_LABELS = 'project = JRASERVER AND issuetype = Suggestion AND status = Closed AND labels is not EMPTY AND component is EMPTY'
+CLOSED_SUGGESTIONS_WITH_LABELS_AND_COMPONENTS = 'project = JRASERVER AND issuetype = Suggestion AND status = Closed AND labels is not EMPTY AND component is not EMPTY'
+GATHERING_INTERESTS_SUGGESTIONS = 'project = JRASERVER AND issuetype = Suggestion AND status = "Gathering Interest" and labels is EMPTY'
+GATHERING_INTERESTS_SUGGESTIONS_WITH_LABLES = 'project = JRASERVER AND issuetype = Suggestion AND status = "Gathering Interest" and labels is not EMPTY'
+OTHERS_SUGGESTIONS = 'project = JRASERVER AND issuetype = Suggestion AND status in ("In Progress", Reviewing, "Under Consideration", "Future Consideration", "Not Being Considered", "Waiting for Release")'
+VULNERABILITIES ='project = JRASERVER AND issuetype = "Public Security Vulnerability"'
 
 ALL_FILTERS = [ 
-                B_CLSD_LOW_NEC_EFV, B_CLSD_LOW_NEC_NEFV, B_CLSD_LOW_EC, 
-                B_CLSD_MED_EC, B_CLSD_MED_NEC, B_CLSD_HIGH, B_NT, B_IN_OTHER_S,
-                S_CLSD_EL_NEC, S_CLSD_EL_EC, S_CLSD_NEL_EC, S_CLSD_NEL_NEL,
-                S_GI_EL, S_GI_NEL, S_IN_OTHER_S, ALL_VULNARAB
+                CLOSED_LOW_BUGS_WITH_COMPONENTS, CLOSED_LOW_BUGS_WITH_COMPONENTS_AND_FIXES, CLOSED_LOW_BUGS, 
+                CLOSED_MEDIUM_BUGS, CLOSED_MEDIUM_BUGS_WITH_COMPONENTS, CLOSED_HIGH_BUGS, NEEDS_TRIAGE_BUGS, 
+                OTHERS_BUGS, CLOSED_SUGGESTIONS_WITH_COMPONENTS, CLOSED_SUGGESTIONS, CLOSED_SUGGESTIONS_WITH_LABELS, 
+                CLOSED_SUGGESTIONS_WITH_LABELS_AND_COMPONENTS, GATHERING_INTERESTS_SUGGESTIONS,
+                GATHERING_INTERESTS_SUGGESTIONS_WITH_LABLES, OTHERS_SUGGESTIONS, VULNERABILITIES
                 ]
--- getting_issue.py
+++ getting_issue.py
@@ -9,7 +9,7 @@ FIELDS = 'summary,issuetype,status,priority,resolution,description,\
         votes,created,updated,issuelinks,comment,fixVersions,versions'
 
 
-def multi_getattr(obj, attr, default=None):
+def nested_gettatr(obj, attr, default=None):
     """Applying several attributes to one object and return default value if attribur does not exist."""
     attributes = attr.split(".")
     for i in attributes:
@@ -25,7 +25,7 @@ def multi_getattr(obj, attr, default=None):
 
 def get_date_from_field(issue, attributes):
     """Getting date data from issue if exist and return as a date object."""
-    value = multi_getattr(issue, attributes, 'None')
+    value = nested_gettatr(issue, attributes, 'None')
     if isinstance(value, str):
         return value
     return datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f%z').date()
@@ -38,9 +38,9 @@ def get_issue_data(issue):
         'type': issue.fields.issuetype.name,
         'summary': issue.fields.summary,
         'status': issue.fields.status.name,
-        'priority': multi_getattr(issue, 'fields.priority.name','None'),
-        'resolution': multi_getattr(issue, 'fields.resolution.name','None'),
-        'description': multi_getattr(issue, 'fields.description', 'None'),
+        'priority': nested_gettatr(issue, 'fields.priority.name','None'),
+        'resolution': nested_gettatr(issue, 'fields.resolution.name','None'),
+        'description': nested_gettatr(issue, 'fields.description', 'None'),
         'votes': issue.fields.votes.votes,
         'watchers': jira.watchers(issue).watchCount,
         'created': get_date_from_field(issue,'fields.created'),
@@ -56,7 +56,7 @@ def get_issue_data(issue):
 def get_issue_links(issue):
     """Getting all links from issue and return as a list of dicts."""
     links = issue.fields.issuelinks
-    issue_links = [ {'linktype' : link.type.name, 'outwardIssue' : multi_getattr(link, 'outwardIssue.key','None')} for link in links]
+    issue_links = [ {'linktype' : link.type.name, 'outwardIssue' : nested_gettatr(link, 'outwardIssue.key','None')} for link in links]
     return issue_links
 
 
@@ -73,17 +73,17 @@ def get_issue_comments(issue):
 
 def get_issue_version(issue):
     """Getting all versions from issue and return max value."""
-    affects_versions = [getattr(fversion,'name','None') for fversion in multi_getattr(issue, 'fields.fixVersions', 'None')]
-    fixed_versions = [getattr(aversion,'name','None') for aversion in multi_getattr(issue, 'fields.versions', 'None')]
+    affects_versions = [getattr(fversion,'name','None') for fversion in nested_gettatr(issue, 'fields.fixVersions', 'None')]
+    fixed_versions = [getattr(aversion,'name','None') for aversion in nested_gettatr(issue, 'fields.versions', 'None')]
     return {'fixed' : max(fixed_versions, default='None'), 'affected' : max(affects_versions, default='None')}
 
 
-def getting_issues_by_filter(jql):
+def get_issues_by_filter(jql):
     """Getting all issues from jql filter and return their data as a list."""
-    issues = [ get_issue_data(issue) for issue in jira.search_issues(f'{jql}', maxResults=False, fields=FIELDS)]
+    issues = [get_issue_data(issue) for issue in jira.search_issues(f'{jql}', maxResults=False, fields=FIELDS)]
     return issues
 
 
 if __name__ == "__main__":
     for jql in fl.ALL_FILTERS:
-        getting_issues_by_filter(jql)
+        get_issues_by_filter(jql)
