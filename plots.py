f96ac4bb698aa9c39dffa8838fd2467c378aee8d
Alexey Lonshakov <lonshakovaleksei@gmail.com>
2021-07-14 00:47:18 +0600

    some fixies after review on 07/13/2021

--- plots.py
+++ plots.py
@@ -5,15 +5,15 @@ import os
 
 PATH = 'figs'
 
+
 def write_html_to_file(filename, dataframe):
     """Writing dataframe to HTML file"""
     html = dataframe.to_html(index=False)
-    text_file = open(f'{PATH}/{filename}.html', "w")
-    text_file.write(html)
-    text_file.close()
+    with open(f'{PATH}/{filename}.html', "w") as text_file:
+        text_file.write(html)
 
 
-def most_voted_suggestions_table():
+def create_most_voted_suggestions_table():
     """Creating a table in html format with data on the most voted suggestions"""
     most_voted_suggestions = pd.DataFrame(
         rd.most_voted_issues('Suggestion', 10), 
@@ -22,7 +22,7 @@ def most_voted_suggestions_table():
     write_html_to_file('most_voted_suggestions', most_voted_suggestions)
 
 
-def most_voted_bugs_table(): 
+def create_most_voted_bugs_table(): 
     """Creating a table in html format with data on the most voted bugs"""
     most_voted_bugs = pd.DataFrame(
         rd.most_voted_issues('Bug', 10), 
@@ -31,7 +31,7 @@ def most_voted_bugs_table():
     write_html_to_file('most_voted_bugs', most_voted_bugs)
 
 
-def most_watched_suggestions_table():
+def create_most_watched_suggestions_table():
     """Creating a table in html format with data on the most watched suggestions"""
     most_watched_suggestions = pd.DataFrame(
         rd.most_watched_issues('Suggestion', 10),
@@ -40,7 +40,7 @@ def most_watched_suggestions_table():
     write_html_to_file('most_watched_suggestions', most_watched_suggestions)
 
 
-def most_watched_bugs_table():
+def create_most_watched_bugs_table():
     """Creating a table in html format with data on the most watched bugs"""
     most_watched_bugs = pd.DataFrame(
         rd.most_watched_issues('Bug', 10),
@@ -49,7 +49,7 @@ def most_watched_bugs_table():
     write_html_to_file('most_watched_bugs', most_watched_bugs)
 
 
-def suggestion_in_status_pie():
+def create_suggestion_in_status_pie():
     """Creating a pie chart in png format with data on the percentage suggestion statuses"""
     suggestion_in_status = pd.DataFrame(rd.count_issues_in_status('Suggestion'), columns =['Status', 'Number of issues'])
     suggestion_in_status['Percentage'] = round(
@@ -62,12 +62,14 @@ def suggestion_in_status_pie():
         kind="pie",
         autopct='%1.1f%%',
         title="Statuses of Suggestions",
-        figsize=(10, 10)
+        figsize=(10, 10),
+        legend=False
         )
+    plt.ylabel('')
     pie_diagram.figure.savefig(f'{PATH}/suggestion_in_status_pie')
 
 
-def bug_in_status_pie():
+def create_bug_in_status_pie():
     """Creating a pie chart in png format with data on the percentage bugs statuses"""
     bug_in_status = pd.DataFrame(rd.count_issues_in_status('Bug'), columns =['Status', 'Number of issues'])
     bug_in_status['Percentage'] = round(
@@ -80,12 +82,14 @@ def bug_in_status_pie():
         kind="pie",
         autopct='%1.1f%%',
         title="Statuses of Bugs",
-        figsize=(10, 10)
+        figsize=(10, 10),
+        legend=False
         )
+    plt.ylabel('')
     pie_diagram.figure.savefig(f'{PATH}/bug_in_status_pie')
 
 
-def completed_issue_destribution_bar():
+def create_completed_issue_destribution_bar():
     """Creating a bar chart in png format with data on the issues distribution by versions"""
     completed_issue_destribution = pd.DataFrame(
         rd.distribution_of_issues_by_fixed_versions(), 
@@ -108,11 +112,16 @@ def completed_issue_destribution_bar():
 
 
 if __name__ == "__main__":
+    functions_to_call = [
+        create_most_voted_suggestions_table,
+        create_most_voted_bugs_table,
+        create_most_watched_suggestions_table,
+        create_most_watched_bugs_table,
+        create_suggestion_in_status_pie,
+        create_bug_in_status_pie,
+        create_completed_issue_destribution_bar
+        ]
+
     os.makedirs(PATH,exist_ok=True)
-    most_voted_suggestions_table()
-    most_voted_bugs_table()
-    most_watched_suggestions_table()
-    most_watched_bugs_table()
-    suggestion_in_status_pie()
-    bug_in_status_pie()
-    completed_issue_destribution_bar()
+    for function in functions_to_call:
+        function()
